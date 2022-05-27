ab111388a89befa277d1161c682342b71bb86944
Sina Rabiee <sinarabiee80@gmai.com>
2022-05-27 23:53:03 +0430

    Q2 sorting

--- include/q1.h
+++ include/q1.h
@@ -1,4 +1,22 @@
 #ifndef Q1_H
 #define Q1_H
 
-#endif //Q1_H
+#include <cmath>
+#include <functional>
+#include <iostream>
+namespace q1 {
+
+using Func = double (*)(const double);
+
+double gradient_descent(double ivalue, double step, Func func)
+{
+    for (size_t i {};; i++) {
+        double x { func(ivalue + i * step) };
+        double y { func(ivalue + (i + 1) * step) };
+        }
+    std::cout << "Gradient Descent" << std::endl;
+    return 2.0;
+}
+}
+
+#endif // Q1_H
--- include/q2.h
+++ include/q2.h
@@ -1,4 +1,82 @@
 #ifndef Q2_H
 #define Q2_H
 
-#endif //Q2_H
+#include <fstream>
+#include <iostream>
+#include <queue>
+#include <regex>
+#include <sstream>
+#include <string>
+
+namespace q2 {
+
+struct Patient {
+    Patient(std::string _name, size_t _age, size_t _smokes, size_t _area_q, size_t _alkhol)
+        : name { _name }
+        , age { _age }
+        , smokes { _smokes }
+        , area_q { _area_q }
+        , alkhol { _alkhol }
+
+    {
+    }
+
+    std::string name;
+    size_t age;
+    size_t smokes;
+    size_t area_q;
+    size_t alkhol;
+};
+
+std::vector<Patient> read_file(std::string filename)
+{
+    std::vector<Patient> P {};
+    std::ifstream file("lung_cancer.csv");
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    std::string txt = buffer.str();
+
+    std::regex pattern(R"((\w+)(\s*),(\w+)(\s*),(\d+),(\d+),(\d+),(\d+))"); // using raw string
+    std::smatch match;
+    while (std::regex_search(txt, match, pattern)) {
+        std::string Fname { match[1].str() };
+        std::string Lname { match[3].str() };
+        std::istringstream isag { match[5].str() };
+        std::istringstream issm { match[6].str() };
+        std::istringstream isar { match[7].str() };
+        std::istringstream isal { match[8].str() };
+        size_t ag {};
+        size_t sm {};
+        size_t ar {};
+        size_t al {};
+        isag >> ag;
+        issm >> sm;
+        isar >> ar;
+        isal >> al;
+        P.push_back(Patient(Fname + " " + Lname, ag, sm, ar, al));
+        txt = match.suffix().str();
+    }
+    return P;
+}
+std::vector<Patient> sort(std::vector<Patient> P)
+{
+    std::vector<Patient> new_P {};
+    struct place {
+        bool operator()(const Patient& P1, const Patient& P2) const
+        {
+            size_t x { 3 * (P1.age) + 5 * (P1.smokes) + 2 * (P1.area_q) + 4 * (P1.alkhol) };
+            size_t y { 3 * (P2.age) + 5 * (P2.smokes) + 2 * (P2.area_q) + 4 * (P2.alkhol) };
+            return x < y;
+        }
+    };
+    std::priority_queue<Patient, std::vector<Patient>, place> pq {};
+    for (auto i : P)
+        pq.push(i);
+    while (!pq.empty()) {
+        new_P.push_back(pq.top());
+        pq.pop();
+    }
+    return new_P;
+}
+}
+#endif // Q2_H
