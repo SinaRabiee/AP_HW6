411e1c0fb681c86f14b53fd799583aa48e910893
Sina Rabiee <sinarabiee80@gmai.com>
2022-05-28 19:38:03 +0430

    Q3 done, Q4 started

--- include/q3.h
+++ include/q3.h
@@ -26,9 +26,14 @@ struct Flight {
     size_t connection_times;
     size_t price;
 };
-// herereeeeeeeeeeeeeeeeeeeeeee
+struct comp {
+    bool operator()(const Flight& F1, const Flight& F2)
+    {
+        return (F1.duration) + (F1.connection_times) + 3 * (F1.price) > (F2.duration) + (F2.connection_times) + 3 * (F2.price);
+    }
+};
 
-inline std::priority_queue<Flight, std::vector<Flight>, decltype(comp)> gather_flights(std::string filename)
+inline std::priority_queue<Flight, std::vector<Flight>, comp> gather_flights(std::string filename)
 {
     // reading file
     std::vector<Flight> F {};
@@ -37,29 +42,45 @@ inline std::priority_queue<Flight, std::vector<Flight>, decltype(comp)> gather_f
     buffer << file.rdbuf();
     std::string txt = buffer.str();
 
-    std::regex pattern(R"((\d-\sflight_number:)(\w+)(\s-\s)(duration:)(\d+)(\w)(\d*)(\w*)(\s-\sconnections:\d)(\s-\sconnection_times:)(\d)(\w)(\d*)(\w*)(,*)(\d*)(\w*)(\d*)(\w*)(,*)(\d*)(\w*)(\d*)(\w*)(\s-\sprice:)(\d*))"); // using raw string
+    std::regex pattern(R"((\d-\s)(flight_number)(:)(\w+)(\s-\s)(duration:)(\d+)(\w)(\d*)(\w*)(\s-\sconnections:)(\d)(\s-\sconnection_times:)(\d)(\w)(\d*)(\w*)(,*)(\d*)(h*)(\d*)(m*)(,*)(\d*)(h*)(\d*)(m*)(\s-\sprice:)(\d*))"); // using raw string
     std::smatch match;
     while (std::regex_search(txt, match, pattern)) {
-        std::string Fnum { match[2].str() };
-        std::istringstream isDur { match[3].str() };
-        std::istringstream isCon { match[6].str() };
-        std::istringstream isCont { match[7].str() };
-        std::istringstream isPr { match[8].str() };
-        size_t Dur {};
+        std::string Fnum { match[4].str() };
+        std::istringstream isDurh { match[7].str() };
+        std::istringstream isDurm { match[9].str() };
+        std::istringstream isCon { match[12].str() };
+        std::istringstream isCont1h { match[14].str() };
+        std::istringstream isCont1m { match[16].str() };
+        std::istringstream isCont2h { match[19].str() };
+        std::istringstream isCont2m { match[21].str() };
+        std::istringstream isCont3h { match[24].str() };
+        std::istringstream isCont3m { match[26].str() };
+        std::istringstream isPr { match[29].str() };
+        size_t Durh {};
+        size_t Durm {};
         size_t Con {};
-        size_t Cont {};
+        size_t Cont1h {};
+        size_t Cont1m {};
+        size_t Cont2h {};
+        size_t Cont2m {};
+        size_t Cont3h {};
+        size_t Cont3m {};
         size_t Pr {};
-        isDur >> Dur;
+        isDurh >> Durh;
+        isDurm >> Durm;
         isCon >> Con;
-        isCont >> Cont;
+        isCont1h >> Cont1h;
+        isCont1m >> Cont1m;
+        isCont2h >> Cont2h;
+        isCont2m >> Cont2m;
+        isCont3h >> Cont3h;
+        isCont3m >> Cont3m;
         isPr >> Pr;
-        F.push_back(Flight(Fnum, Dur, Con, Cont, Pr));
+        F.push_back(Flight(Fnum, (Durh * 60) + Durm, Con, (Cont1h + Cont2h + Cont3h) * 60 + Cont1m + Cont2m + Cont3m, Pr));
         txt = match.suffix().str();
     }
-    auto comp {
-        [](const Flight& F1, const Flight& F2) { return (F1.duration) + (F1.connection_times) + 3 * (F1.price) > (F2.duration) + (F2.connection_times) + 3 * (F2.price); }
-    };
-    std::priority_queue<Flight, std::vector<Flight>, decltype(comp)> F_new { comp };
+
+    std::priority_queue<Flight, std::vector<Flight>, comp> F_new {};
     for (auto& i : F)
         F_new.push(i);
     return F_new;
