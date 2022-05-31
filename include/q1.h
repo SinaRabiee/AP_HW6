125fed6cb503f68046d9d36ea94579c80e779d70
Sina Rabiee <sinarabiee80@gmai.com>
2022-05-31 15:30:53 +0430

    Q1 completed (using templates only)

--- include/q1.h
+++ include/q1.h
@@ -8,9 +8,8 @@
 #include <vector>
 
 namespace q1 {
-
 template <typename T, typename FUNC>
-T gradient_descent(T ivalue, T step, std::function<double(double)> func)
+T gradient_descent(T ivalue, T step, FUNC func = FUNC())
 {
     double x { ivalue };
     double min {};
@@ -29,23 +28,23 @@ T gradient_descent(T ivalue, T step, std::function<double(double)> func)
 }
 
 // std::finction
-inline double gradient_descent(double ivalue, double step, std::function<double(double)> func)
-{
-    double x { ivalue };
-    double min {};
-    while (true) {
-        double p_pre { func(x - step) };
-        double p_now { func(x) };
-        double p_nex { func(x + step) };
-        min = std::min({ p_pre, p_now, p_nex });
-        if (min == p_pre)
-            x -= step;
-        else if (min == p_nex)
-            x += step;
-        else
-            return x;
-    }
-}
+// inline double gradient_descent(double ivalue, double step, std::function<double(double)> func)
+// {
+//     double x { ivalue };
+//     double min {};
+//     while (true) {
+//         double p_pre { func(x - step) };
+//         double p_now { func(x) };
+//         double p_nex { func(x + step) };
+//         min = std::min({ p_pre, p_now, p_nex });
+//         if (min == p_pre)
+//             x -= step;
+//         else if (min == p_nex)
+//             x += step;
+//         else
+//             return x;
+//     }
+// }
 }
 
 #endif // Q1_H
